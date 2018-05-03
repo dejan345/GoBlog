a4d4c9c4403a9962d3b4b868a9799a313d101ae0
dejan345 <dejan345@gmail.com>
2018-05-03 21:31:08 +0200

    Setup gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+// Include gulp
+var gulp = require("gulp");
+
+// Include plugins
+var size = require("gulp-size");
+var notify = require("gulp-notify");
+var cssmin = require("gulp-cssmin");
+var htmlmin = require("gulp-htmlmin");
+
+// gulp.task("move", () => gulp.src("public/**/**.*").pipe(gulp.dest("dist")));
+
+// Html minify task
+gulp.task("htmlmin", function() {
+  return gulp
+    .src("public/**/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("dist/"));
+});
+
+// Css minify task
+gulp.task("cssmin", function() {
+  gulp
+    .src("public/css/*.css")
+    .pipe(cssmin())
+    .pipe(gulp.dest("dist/css"));
+});
+
+// Default task
+gulp.task("default", ["cssmin"]);
